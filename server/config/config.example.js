55dbce96a3525ead6aec25bfb637001bef98e26e
iracoon <podarallarachana@ufl.edu>
2020-02-08 10:43:31 -0600

    test commit to make sure git is working

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -2,6 +2,8 @@
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
+
+//I am just checking with this statement that git is working as it should!
 export default {
   db: {
     uri: ''//place the URI of your mongo database here.
